4ce57f04d69d7759adcbbcdd248c5d80507f48fb
Tuomas Suutari <tuomas.suutari@anders.fi>
2024-12-11 10:57:25 +0200

    db: Allow configuring database via URL
    Make it possible to configure the database connection with an URL, since
    that is usually simpler to specify than the 5 different values for user,
    password, host, port and name.

--- src/passari_workflow/db/connection.py
+++ src/passari_workflow/db/connection.py
@@ -11,11 +11,19 @@ def get_connection_uri():
     """
     Get the connection URI used to connect to the database
     """
+    url = CONFIG["db"].get("url")
     user = CONFIG["db"].get("user")
     password = CONFIG["db"].get("password", "")
     host = CONFIG["db"].get("host")
     port = CONFIG["db"].get("port")
-    name = CONFIG["db"]["name"]
+    name = CONFIG["db"].get("name")
+
+    if url and not name:
+        return url
+    elif not url and not name:
+        raise EnvironmentError("Either 'url' or 'name' is required for db")
+    elif url and name:
+        raise EnvironmentError("The db 'url' and 'name' are exclusive")
 
     if user and host and port:
         return f"postgresql://{user}:{quote_plus(password)}@{host}:{port}/{name}"
